834d009c3c193f31d280640da4f6df8901123faa
bbaktaeho <lth9036@gmail.com>
2020-11-02 13:47:37 +0900

    Init: 초기화

---
+++ app/src/main/java/com/example/wordmemorizationgame/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wordmemorizationgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // MainActivity
+        // develop
+        // branch
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
