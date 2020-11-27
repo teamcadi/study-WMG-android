2000abfa052bb847ea04b37d7afa24a8b392981a
soohyun <soohyun6879@naver.com>
2020-11-27 21:49:43 +0900

    Add: 하단바 추가

--- app/src/main/java/com/example/wordmemorizationgame/ui/SplashActivity.java
+++ app/src/main/java/com/example/wordmemorizationgame/ui/SplashActivity.java
@@ -27,7 +27,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //
-                Intent intent = new Intent(getApplicationContext(),UserActivity.class);
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/example/wordmemorizationgame/ui/main/MainActivity.java
+++ app/src/main/java/com/example/wordmemorizationgame/ui/main/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.wordmemorizationgame.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.wordmemorizationgame.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // MainActivity
-        // develop
-        // branch
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navi);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHost_main);
+        NavigationUI.setupWithNavController(bottomNavigationView,navHostFragment.getNavController());
     }
 }
